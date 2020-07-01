67ca9cbf0b7672e86a16d17a13d7db321fcf8fb5
Joe C <jcheng1287@gmail.com>
2020-06-30 20:53:03 -0400

    modified df and merge to be index-based

--- legal_check.py
+++ legal_check.py
@@ -13,6 +13,39 @@ Reaccruals files must contain named: ['Vendor ID', 'Invoice Number']
 Onit files must contain named: ['Vendor ID', 'Invoice Number']
 '''
 
+def prepare_files(dir_path,vendor_file_name, onit_file_name):
+    #VC Confirm file
+    #['Vendor ID', 'Invoice Number]
+    vc_df = pd.read_csv(os.path.join(dir_path,vendor_file_name),dtype={'Vendor ID':'object','Invoice Number':'object'}, index_col=['Vendor ID', 'Invoice Number'])
+    vc_df = vc_df.sort_index(level=vc_df.index.names)
+    
+    #Onit file
+    #['Vendor ID', 'Invoice Number']
+    onit_df = pd.read_csv(os.path.join(dir_path,onit_file_name),dtype={'Vendor ID':'object','Invoice Number':'object'}, index_col=['Vendor ID', 'Invoice Number'])
+    
+    #confirm final df
+    vc_final = vc_df.copy()
+    vc_final['Amount'] = vc_final['Amount'].apply(lambda x: x.replace(',','')) #necessary step to convert to amount to float
+    vc_final = vc_final.astype({'Amount':'float64'})
+
+    # onit final df
+    onit_final = onit_df.copy()
+    onit_final['Invoice Total (USD)'] = onit_final['Invoice Total (USD)'].apply(lambda x: x.replace(',','')) #necessary step to convert to amount to float
+    onit_final = onit_final.astype({'Invoice Total (USD)':'float64'})
+
+    #create check files for onit and vc
+    onit_check = onit_df.copy()
+    onit_check[onit_file_name] = True
+    vc_check = vc_df.copy()
+    vc_check[vendor_file_name] = True
+
+    #merge onit check with vc_final and vc check with onit final
+    vc_final = vc_final.merge(onit_check[onit_file_name], on = ['Vendor ID', 'Invoice Number'], how='left') #merge onit with confirm df
+    onit_final = onit_final.merge(vc_check[vendor_file_name], on = ['Vendor ID', 'Invoice Number'], how='left') #merge vc with onit df
+
+    return [vc_final, onit_final]
+
+
 def legal_match():
 
     '''
@@ -53,54 +86,32 @@ def legal_match():
 
     print('# of unknown files: ' + str(unknown_filetype))
 
-    #VC Confirm file
-    #['Vendor ID', 'Invoice Number]
-    vc_df = pd.read_csv(os.path.join(dir_path,CONFIRMS),dtype={'Vendor ID':'object','Invoice Number':'object'})
+    # vc_final, onit_final = 
+    vc_final,onit_final = prepare_files(dir_path,CONFIRMS,ONIT)
     
-    #Onit file
-    #['Vendor ID', 'Invoice Number']
-    onit_df = pd.read_csv(os.path.join(dir_path,ONIT),dtype={'Vendor ID':'object','Invoice Number':'object'})
     
-    #confirm final df
-    vc_final = vc_df.copy()
-    vc_final['Amount'] = vc_final['Amount'].apply(lambda x: x.replace(',','')) #necessary step to convert to amount to float
-    vc_final = vc_final.astype({'Amount':'float64'})
-
-    # onit final df
-    onit_final = onit_df.copy()
-    onit_final['Invoice Total (USD)'] = onit_final['Invoice Total (USD)'].apply(lambda x: x.replace(',','')) #necessary step to convert to amount to float
-    onit_final = onit_final.astype({'Invoice Total (USD)':'float64'})
-
-    #create check files for onit and vc
-    onit_check = onit_df.copy()
-    onit_check[ONIT] = True
-    vc_check = vc_df.copy()
-    vc_check[CONFIRMS] = True
-
-    #merge onit check with vc_final and vc check with onit final
-    vc_final = vc_final.merge(onit_check[['Vendor ID', 'Invoice Number', ONIT]], on=['Vendor ID','Invoice Number'], how='left') #merge onit with confirm df
-    onit_final = onit_final.merge(vc_check[['Vendor ID', 'Invoice Number', CONFIRMS]], on=['Vendor ID','Invoice Number'], how='left') #merge vc with onit df
-        
     # acm bills
     # ['Vendor','Vendor Ref.']
     for file in ACMBILLS:
         file_df = pd.read_csv(os.path.join(dir_path,file), dtype={'Vendor':'object','Vendor Ref.':'object'})
         file_df.rename(columns={'Vendor':'Vendor ID', 'Vendor Ref.':'Invoice Number'}, inplace=True)
+        file_df.set_index(['Vendor ID', 'Invoice Number'], inplace=True)
         file_df[file] = True
 
         #merge with final dfs
-        vc_final = vc_final.merge(file_df[['Vendor ID','Invoice Number', file]], on=['Vendor ID','Invoice Number'], how='left')
-        onit_final = onit_final.merge(file_df[['Vendor ID','Invoice Number', file]], on=['Vendor ID','Invoice Number'], how='left')
+        vc_final = vc_final.merge(file_df[file], on=['Vendor ID','Invoice Number'], how='left')
+        onit_final = onit_final.merge(file_df[file], on=['Vendor ID','Invoice Number'], how='left')
 
 
     # stampli accruals
     for file in STAMPLI_ACCRUALS:
         file_df = pd.read_csv(os.path.join(dir_path,file),dtype={'Vendor ID':'object','Invoice Number':'object'})
+        file_df.set_index(['Vendor ID', 'Invoice Number'], inplace=True)
         file_df[file] = True
 
         #merge with confirm df
-        vc_final = vc_final.merge(file_df[['Vendor ID','Invoice Number', file]], on =['Vendor ID','Invoice Number'], how='left')
-        onit_final = onit_final.merge(file_df[['Vendor ID','Invoice Number', file]], on=['Vendor ID','Invoice Number'], how='left')
+        vc_final = vc_final.merge(file_df[file], on =['Vendor ID','Invoice Number'], how='left')
+        onit_final = onit_final.merge(file_df[ file], on=['Vendor ID','Invoice Number'], how='left')
 
 
     # stampli reaccruals
@@ -110,10 +121,11 @@ def legal_match():
 
         #rename columns in ACM bills
         file_df = file_df.rename(columns={'Max. GL Rerefence Number':'Vendor ID'})
+        file_df.set_index(['Vendor ID', 'Invoice Number'], inplace=True)
 
         #merge with confirm df
-        vc_final = vc_final.merge(file_df[['Vendor ID','Invoice Number', file]], on=['Vendor ID','Invoice Number'], how='left')
-        onit_final = onit_final.merge(file_df[['Vendor ID','Invoice Number', file]], on=['Vendor ID','Invoice Number'], how='left')
+        vc_final = vc_final.merge(file_df[file], on=['Vendor ID','Invoice Number'], how='left')
+        onit_final = onit_final.merge(file_df[file], on=['Vendor ID','Invoice Number'], how='left')
 
 
     #count times invoice appears in source docs and return if they're accrued
@@ -138,4 +150,8 @@ def legal_match():
     vc_final_pv= vc_final.pivot_table(index='Vendor Name', columns = ['Unbilled/Unpaid','Accrued?','Currency'], values='Amount', aggfunc=np.sum, margins=True)
     onit_final_pv= onit_final.pivot_table(index='ACM Vendor Name', columns = ['Accrued?'], values='Invoice Total (USD)', aggfunc=np.sum, margins=True)
 
-    return {'vc_final':vc_final,'onit_final':onit_final,'vc_final_pv':vc_final_pv, 'onit_final_pv':onit_final_pv}
+    return {'vc_final':vc_final,'onit_final':onit_final,'vc_final_pv':vc_final_pv, 'onit_final_pv': onit_final_pv}
+
+
+
+
